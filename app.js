0479ae934cf850ac5e7028c0927582db70895737
Yosua1011 <yosua1011@gmail.com>
2017-09-13 14:11:30 +0700

    login and registration page done

--- app.js
+++ app.js
@@ -1,46 +1,34 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+// var session = require('express-session')
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 
-var app = express();
+app.set('view engine', 'ejs')
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(session({
+//     secret: 'kitfox-2017',
+//     cookie: {}
+// }))
+app.use(express.static(__dirname + '/public'));
+// app.use(express.static('public'))
 
-app.use('/', index);
-app.use('/users', users);
+//==================Call Routes===============================//
+let index = require('./routes/index.js')
+// let teachers = require('./routes/teachers.js')
+// let subjects = require('./routes/subjects.js')
+// let students = require('./routes/students.js')
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// ===================== routing =================================//
+app.use('/', index)
+// app.use('/teachers', teachers)
+// app.use('/subjects', subjects)
+// app.use('/students', students)
+// =================== end of routing  ========================== //
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Example app listening on port 3000!')
+})
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: 'Login Page' });
 });
+
+//Register
+router.get('/register', (req, res) => {
+  res.render('register', {title: 'Register', error_reg: false})
+})
 
 module.exports = router;
