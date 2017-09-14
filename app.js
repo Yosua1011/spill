6b076bb14ddb31ca67548de02a584f474ea6e317
Yosua1011 <yosua1011@gmail.com>
2017-09-14 21:35:53 +0700

    spill-kitfox file init

--- app.js
+++ app.js
@@ -17,16 +17,9 @@ app.use(express.static(__dirname + '/public'));
 
 //==================Call Routes===============================//
 let index = require('./routes/index.js')
-// let teachers = require('./routes/teachers.js')
-// let subjects = require('./routes/subjects.js')
-// let students = require('./routes/students.js')
 
 // ===================== routing =================================//
 app.use('/', index)
-// app.use('/teachers', teachers)
-// app.use('/subjects', subjects)
-// app.use('/students', students)
-// =================== end of routing  ========================== //
 
 
 app.listen(process.env.PORT || 3000, function () {
